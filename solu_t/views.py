5d184f2c5a221092387a6d624687b9e41a0a6e0f
Room-201-dev <room201art@gmail.com>
2022-12-21 02:27:16 +0900

    change settings and signup template

--- solu_t/views.py
+++ solu_t/views.py
@@ -16,6 +16,17 @@ from .models import ApplyList, ApplyData, Notice, CustomUser, ContactData
 from .forms import SorryForm, ApplyBaseForm, ApplyCustomizeForm, SignUpForm, ContactForm, LoginForm, BasePlusWorkForm, \
     BaseRefreshDayForm, CustomPlusWorkForm, CustomRefreshDayForm
 
+from django.views.decorators.csrf import requires_csrf_token
+from django.http import HttpResponseServerError
+
+
+@requires_csrf_token
+def my_customized_server_error(request, template_name='500.html'):
+    import sys
+    from django.views import debug
+    error_html = debug.technical_500_response(request, *sys.exc_info()).content
+    return HttpResponseServerError(error_html)
+
 
 class SignUpView(FormView):
     form_class = SignUpForm
