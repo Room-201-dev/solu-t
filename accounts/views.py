e466cfff33d00bc8d260f3130eb9c5dfb94a8879
Room-201-dev <room201art@gmail.com>
2022-12-26 17:57:05 +0900

    add paid leave

--- accounts/views.py
+++ accounts/views.py
@@ -120,17 +120,18 @@ class NoticeEditView(LoginRequiredMixin, View):
         notice_edit_form = NoticePostForm(
             request.POST or None,
             initial={
-                'choice_base': notice_detail.base,
-                'choice_shift': notice_detail.shift,
-                'choice_important': notice_detail.important,
                 'title': notice_detail.title,
                 'content': notice_detail.content,
             }
         )
-        notice_base = notice_detail.base
+        # notice_base = notice_detail.base
+        # notice_shift = notice_detail.shift
+        # notice_important = notice_detail.important
         return render(request, 'accounts/admin_notice_edit.html', {
             'notice_edit_form': notice_edit_form,
-            'notice_base': notice_base
+            # 'notice_base': notice_base,
+            # 'notice_shift': notice_shift,
+            # 'notice_important': notice_important
         })
 
     def post(self, request, *args, **kwargs):
@@ -140,9 +141,6 @@ class NoticeEditView(LoginRequiredMixin, View):
             notice_data = Notice.objects.get(id=self.kwargs['pk'])
             notice_data.title = form.cleaned_data['title']
             notice_data.content = form.cleaned_data['content']
-            notice_data.important = form.cleaned_data['choice_important']
-            notice_data.base = form.cleaned_data['choice_base']
-            notice_data.shift = form.cleaned_data['choice_shift']
             notice_data.tag = form.cleaned_data['choice_tag']
 
             notice_data.save()
